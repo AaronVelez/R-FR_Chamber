f32c8cc49bce7f51c651b4fa23070d192733c3f8
Aarón I. Vélez Ramírez <aaron.velez@comunidad.unam.mx>
2021-12-14 22:20:30 -0600

    Visual Studio Solution created

---
+++ Main/Main.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Main.ino
+ Created:	12/14/2021 10:19:58 PM
+ Author:	aivel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
